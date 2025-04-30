cda41ad3d077637ca6455f84ea4084601a724dbe
lemosFTW <rl.fernandes@campus.fct.unl.pt>
2025-04-30 13:32:53 -0300

    Add some migrations to allow Roles feature, and implemented the service to handle the access only to admin users

--- backend/src/modules/api/users/users.controller.ts
+++ backend/src/modules/api/users/users.controller.ts
@@ -1,24 +1,36 @@
-import { Controller, UseGuards, UseInterceptors, UploadedFile, Request, Get, Delete, Param, Body } from '@nestjs/common';
+import { Controller, UseGuards, Get,Request, ForbiddenException} from '@nestjs/common';
 import { JwtAuthGuard } from '../../auth/guards/jwt-auth.guard';
 import { UsersService } from './users.service';
+import { RoleService } from '../../auth/role.service';
 @Controller('api/users')
 export class UsersController {
     constructor(
         private usersService: UsersService,
+        private roleService: RoleService,
     ) {}
 
 
     @Get()
     @UseGuards(JwtAuthGuard)
-    async getAllUsers() {
-        return (await this.usersService.listAllUsers()).map(user => {
+    async getAllUsers( @Request() req: { user: { id: string } }) {
+
+        if (!(await this.roleService.isAdmin(req.user.id))) 
+            throw new ForbiddenException('You do not have permission to access this resource');
+
+        const usersFromDb = await this.usersService.listAllUsers();
+
+        const userPromises = usersFromDb.map(async (user) => {
+            const roleName = await this.roleService.roleIdToString(user.roleId);
             return {
                 id: user.id,
                 name: user.name,
                 email: user.email,
-                createdAt: user.createdAt
-                }
-            }
-        )
+                createdAt: user.createdAt,
+                role: roleName,
+            };
+        });
+        const usersWithRoles = await Promise.all(userPromises);
+
+        return usersWithRoles;
     }
 }
--- backend/src/modules/api/users/users.module.ts
+++ backend/src/modules/api/users/users.module.ts
@@ -3,6 +3,7 @@ import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { RoleService } from 'src/modules/auth/role.service';
 @Module({
     imports: [],
     controllers: [
@@ -10,7 +11,8 @@ import { PrismaService } from 'src/prisma/prisma.service';
     ],
     providers: [
         UsersService,
-        PrismaService
+        PrismaService,
+        RoleService
     ],
     exports: [],
 })
